c136fefd0f36738561600100424eedbeea614d5b
Vitor Bertti <berttivitor@gmail.com>
2020-04-26 15:54:37 -0300

    Created mongodb connection

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,21 @@
 import express, { json } from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+require('dotenv').config();
 
 import routes from './routes';
 
 const app = express();
+
+mongoose
+   .connect(<string>process.env.MONGODB_CONNECTION_STRING, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+   })
+   .then(() => console.log('Connected'))
+   .catch((err: Error) => console.log('Error', err.stack));
+
+app.use(cors());
 app.use(json());
 app.use(routes);
 
